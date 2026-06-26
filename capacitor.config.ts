fc5f95ac9d8e43dfd86d3b5e39552447141584e1
beingshasahn <135945540+beingshasahn@users.noreply.github.com>
2026-06-26 17:54:00 +0300

    Create capacitor.config.ts

---
+++ capacitor.config.ts
@@ -0,0 +1,13 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+
+appId: "com.korosho.school",
+
+appName: "Korosho School System",
+
+webDir: "."
+
+};
+
+export default config;
